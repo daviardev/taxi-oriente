42ef109a282c84844ec060e228196de1e1c64d51
daviardev <jerson.silva99@hotmail.com>
2023-04-30 22:18:57 -0500

    Create form driver page

--- src/pages/formulario-conductor/index.js
+++ src/pages/formulario-conductor/index.js
@@ -3,21 +3,85 @@ import Head from 'next/head'
 import Logo from '@/components/Logo'
 
 import { FaUserPlus } from 'react-icons/fa'
+import { IoIosArrowDroprightCircle } from 'react-icons/io'
 
 import styles from '@/styles/FormDriver.module.css'
+import Button from '@/components/Button'
 
 export default function FormDriver () {
+  const handleClick = e => {
+    e.preventDefault()
+  }
   return (
     <>
       <Head>
         <title>Formulario conductor - Taxis Oriente S.A.S</title>
         <link rel='icon' href='/logo-taxiapp.png' />
       </Head>
-      <div className={styles.contentApp}>
-        <Logo />
-        <FaUserPlus
-          className={styles.iconUser}
-        />
+      <div className={styles.container}>
+        <center>
+          <Logo />
+          <FaUserPlus
+            className={styles.iconUser}
+          />
+        </center>
+        <center>
+          <div className={styles.content}>
+            <form action='#'>
+              <div className={styles.userDetails}>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Nombres</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Apellidos</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Departamento</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Ciudad</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Teléfono</span>
+                  <input type='text' equired />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Documento</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Fecha de nacimiento</span>
+                  <input type='date' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Correo electrónico</span>
+                  <input type='email' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Contraseña</span>
+                  <input type='password' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Repetir contraseña</span>
+                  <input type='password' required />
+                </div>
+              </div>
+
+              <div className={styles.button}>
+                <center>
+                  <Button onClick={handleClick}>
+                    Siguiente
+                    <IoIosArrowDroprightCircle className={styles.arrow} />
+                  </Button>
+                </center>
+              </div>
+            </form>
+          </div>
+        </center>
       </div>
     </>
   )
