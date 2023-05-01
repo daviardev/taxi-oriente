cdf47101e54a0c60315dac321617c29065aa922c
daviardev <jerson.silva99@hotmail.com>
2023-05-01 16:32:37 -0500

    Create form vehicle

---
+++ src/pages/formulario-conductor-vehiculo/index.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+
+import Logo from '@/components/Logo'
+import Button from '@/components/Button'
+
+import { FaUserPlus } from 'react-icons/fa'
+import { IoIosArrowDroprightCircle } from 'react-icons/io'
+
+import styles from '@/styles/FormDriver.module.css'
+
+export default function FormDriverVehicle () {
+  return (
+    <>
+      <Head>
+        <title>Formulario conductor - Taxis Oriente S.A.S</title>
+        <link rel='icon' href='/logo-taxiapp.png' />
+      </Head>
+      <div className={styles.container}>
+        <center>
+          <Logo />
+          <FaUserPlus
+            className={styles.iconUser}
+          />
+        </center>
+        <center>
+          <div className={styles.content}>
+            <form action='#'>
+              <div className={styles.userDetails}>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>No. licencia</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Placa del vehículo</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Marca del vehículo</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Modelo del vehículo</span>
+                  <input type='text' required />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Año</span>
+                  <input type='text' equired />
+                </div>
+                <div className={styles.inputBox}>
+                  <span className={styles.details}>Matricula del vehículo</span>
+                  <input type='text' required />
+                </div>
+              </div>
+
+              <div className={styles.button}>
+                <center>
+                  <Button>
+                    Siguiente
+                    <IoIosArrowDroprightCircle className={styles.arrow} />
+                  </Button>
+                </center>
+              </div>
+            </form>
+          </div>
+        </center>
+      </div>
+    </>
+  )
+}
